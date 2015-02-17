dbb1a0b74b129cba15b0ec5b32d1c5d7099c371a
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2015-02-17 19:34:43 +0100

    Test after :( OpenStreetMap

--- src/main/java/fr/xebia/di/Coordinate.java
+++ src/main/java/fr/xebia/di/Coordinate.java
@@ -8,4 +8,33 @@ public class Coordinate {
         this.latitude = latitude;
         this.longitude = longitude;
     }
+
+    @Override
+    public String toString() {
+        return "Coordinate{" +
+                "latitude=" + latitude +
+                ", longitude=" + longitude +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordinate that = (Coordinate) o;
+
+        return Double.compare(that.latitude, latitude) == 0 && Double.compare(that.longitude, longitude) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        temp = Double.doubleToLongBits(latitude);
+        result = (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(longitude);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
 }
---
+++ src/test/java/fr/xebia/di/geocoding/OpenStreetMapGeocodingIT.java
@@ -0,0 +1,18 @@
+package fr.xebia.di.geocoding;
+
+import fr.xebia.di.Coordinate;
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OpenStreetMapGeocodingIT {
+    @Test
+    public void testGeocode() {
+        Optional<Coordinate> coordinate = new OpenStreetMapGeocoding().geocode("156 Boulevard Haussmann, 75008 Paris, France");
+        
+        assertThat(coordinate.isPresent()).isTrue();
+        assertThat(coordinate.get()).isEqualTo(new Coordinate(48.875264, 2.3110709));
+    }
+}
